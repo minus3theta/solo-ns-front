0db25613a66b6e70ec453f79cb96f6bc0ca85108
minus3theta <yone_j_synthesis@yahoo.co.jp>
2023-07-16 08:23:27 +0900

    Add auth to page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,15 @@
+"use client";
 import Image from 'next/image'
+import { Amplify } from 'aws-amplify';
+import type { WithAuthenticatorProps } from '@aws-amplify/ui-react';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 
-export default function Home() {
+import awsconfig from '../src/aws-exports';
+Amplify.configure(awsconfig);
+
+export default withAuthenticator(Home);
+export function Home({ signOut, user }: WithAuthenticatorProps) {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
@@ -8,6 +17,7 @@ export default function Home() {
           Get started by editing&nbsp;
           <code className="font-mono font-bold">app/page.tsx</code>
         </p>
+        <button onClick={signOut}>Sign out</button>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
